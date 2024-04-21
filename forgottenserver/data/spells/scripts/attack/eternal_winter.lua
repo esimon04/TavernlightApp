c91551c8ac2824851e2cf8772b23210822713f38
esimon04 <101423055+esimon04@users.noreply.github.com>
2024-04-21 14:46:12 -0400

    Question5

--- forgottenserver/data/spells/scripts/attack/eternal_winter.lua
+++ forgottenserver/data/spells/scripts/attack/eternal_winter.lua
@@ -1,16 +1,31 @@
-local combat = Combat()
-combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
-combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
-combat:setArea(createCombatArea(AREA_CIRCLE5X5))
+--Callback to continually cause ice toranado animation 'count' times
+function frigoCallback(cid, position, count)
+	if Creature(cid) then
+		if count > 0 then
+			position:sendMagicEffect(CONST_ME_ICETORNADO)
+			doAreaCombat(cid, COMBAT_ICEDAMAGE, position, 0, -100, -100, CONST_ME_ICETORNADO)
+		end
 
-function onGetFormulaValues(player, level, magicLevel)
-	local min = (level / 5) + (magicLevel * 5.5) + 25
-	local max = (level / 5) + (magicLevel * 11) + 50
-	return -min, -max
+		if count < 3 then
+			count = count + 1
+			addEvent(frigoCallback, 1000, cid, position, count)
+		end
+	end
 end
 
-combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+--Needed to set 'count' to 0
+function onTargetTile(creature, position)
+	frigoCallback(creature:getId(), position, 0)
+end
 
-function onCastSpell(creature, variant)
+local combat = Combat()
+--Pretty sure to get the area to be exactly how it was in the question video I would have to take apart the sprite.
+--This was about the best area I could make to match it without doing it.
+combat:setArea(createCombatArea(AREA_DIAMOND))
+--Setting callback
+combat:setCallback(CALLBACK_PARAM_TARGETTILE, "onTargetTile")
+
+
+function onCastSpell(creature, variant, isHotkey)
 	return combat:execute(creature, variant)
 end
